330e08cf92a6ac0ac363a8c695a5650773e33093
HDS07 <hs4205170@gmail.com>
2025-01-04 23:13:34 +0530

    day 2 learning backend and added two more folders with updated work

--- app.js
+++ app.js
@@ -5,11 +5,43 @@ const express =require('express');
 const app =express();
 
 app.set("view engine",'ejs')
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static("public"))
+
+
+// app.use((req,res,next)=>{
+//     console.log("This is middleware");
+//     return next()
+// })
+
+
+
+// app.get('/',(req,res)=>{
+//     res.send('Hello World')
+// })
+
 app.get('/',(req,res)=>{
-    res.send('Hello World')
+    res.render('index')  
 })
+
+
 
 app.get('/about',(req,res)=>{
     res.send('This is about page')
 })
+
+
+// app.get('/get-form-data',(req,res)=>{
+//     console.log(req.query)
+//     res.send('data received')
+// })
+
+
+app.post('/get-form-data',(req,res)=>{
+    console.log(req.body)
+    res.send('data received')
+})
+
 app.listen(3000)
